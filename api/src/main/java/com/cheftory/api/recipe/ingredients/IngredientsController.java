5877953d9ba0719e6df33dd3df370f863620de2b
황교준 <hwangkyojun7806@gmail.com>
2025-07-04 18:37:31 +0900

    (fix) restcontroller에 path가 들어가서 requestmapping에 추가

--- api/src/main/java/com/cheftory/api/recipe/ingredients/IngredientsController.java
+++ api/src/main/java/com/cheftory/api/recipe/ingredients/IngredientsController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/recipeinfos")
 public class IngredientsController {
