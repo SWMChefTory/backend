3c7ca318c1a85ca0bfeeb71acb0f44b92981376a
황교준 <hwangkyojun7806@gmail.com>
2025-07-04 18:36:42 +0900

    (fix) controller에서 restcontroller로 변경

--- api/src/main/java/com/cheftory/api/recipe/info/RecipeInfoController.java
+++ api/src/main/java/com/cheftory/api/recipe/info/RecipeInfoController.java
@@ -3,14 +3,12 @@ package com.cheftory.api.recipe.info;
 import com.cheftory.api.recipe.info.dto.RecipeInfoFindResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RestController("/recipeinfos")
+@RestController
+@RequestMapping("/recipeinfos")
 @RequiredArgsConstructor
 public class RecipeInfoController {
     private final FindRecipeInfoService findRecipeInfoService;
--- api/src/main/java/com/cheftory/api/recipe/step/RecipeStepController.java
+++ api/src/main/java/com/cheftory/api/recipe/step/RecipeStepController.java
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/recipeinfo")
+@RestController
+@RequestMapping("/recipeinfos")
 @RequiredArgsConstructor
 public class RecipeStepController {
     private final FindRecipeStepService findRecipeStepService;
