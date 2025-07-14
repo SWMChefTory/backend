b76f25e62524503ec81acce11e5cd7e94cd0ef36
황교준 <hwangkyojun7806@gmail.com>
2025-07-14 19:53:41 +0900

    fix : swagger 버전 변경

--- src/main/java/com/cheftory/api/common/config/SwaggerConfig.java
+++ src/main/java/com/cheftory/api/common/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ public class SwaggerConfig {
     public GroupedOpenApi boardGroupedOpenApi() {
         return GroupedOpenApi
                 .builder()
-                .group("recipe") // group 설정 (API들을 그룹화시켜 그룹에 속한 API들만 확인할 수 있도록 도와줌)
+                .group("v1") // group 설정 (API들을 그룹화시켜 그룹에 속한 API들만 확인할 수 있도록 도와줌)
                 .pathsToMatch("/**")
                 .addOpenApiCustomizer(
                         openApi ->
