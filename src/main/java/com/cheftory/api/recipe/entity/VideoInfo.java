1caaf0fe638794ad1b85d8bae4a4753205472b47
황교준 <hwangkyojun7806@gmail.com>
2025-07-21 20:48:26 +0900

    chore : 사용하지 않는 import 정리

--- src/main/java/com/cheftory/api/recipe/entity/VideoInfo.java
+++ src/main/java/com/cheftory/api/recipe/entity/VideoInfo.java
@@ -1,6 +1,5 @@
 package com.cheftory.api.recipe.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 import org.springframework.web.util.UriComponents;
