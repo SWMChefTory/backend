3770362658ef8c5e00a28da4252fbd49463c414d
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2025-07-26 01:39:27 +0900

    Update RecipeRepository.java

--- src/main/java/com/cheftory/api/recipe/RecipeRepository.java
+++ src/main/java/com/cheftory/api/recipe/RecipeRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import jakarta.transaction.Transactional;
 import java.net.URI;
 import java.util.UUID;
 
