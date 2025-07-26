8212cb4eba8d462b981b4c2820f5d619796989ab
황교준 <hwangkyojun7806@gmail.com>
2025-07-26 09:04:08 +0900

    fix

--- src/main/java/com/cheftory/api/recipe/caption/entity/converter/SegmentsJsonConverter.java
+++ src/main/java/com/cheftory/api/recipe/caption/entity/converter/SegmentsJsonConverter.java
@@ -2,7 +2,14 @@ package com.cheftory.api.recipe.caption.entity.converter;
 
 import com.cheftory.api._common.GenericJsonConverter;
 import com.cheftory.api.recipe.caption.entity.Segment;
+import com.fasterxml.jackson.core.type.TypeReference;
+import jakarta.persistence.Converter;
 import java.util.List;
 
+@Converter(autoApply = false)
 public class SegmentsJsonConverter extends GenericJsonConverter<List<Segment>> {
+
+  public SegmentsJsonConverter() {
+    super(new TypeReference<List<Segment>>() {});
+  }
 }
--- src/main/java/com/cheftory/api/recipe/step/entity/converter/DetailsJsonConverter.java
+++ src/main/java/com/cheftory/api/recipe/step/entity/converter/DetailsJsonConverter.java
@@ -1,7 +1,13 @@
 package com.cheftory.api.recipe.step.entity.converter;
 
 import com.cheftory.api._common.GenericJsonConverter;
+import com.fasterxml.jackson.core.type.TypeReference;
+import jakarta.persistence.Converter;
 import java.util.List;
 
+@Converter(autoApply = false)
 public class DetailsJsonConverter extends GenericJsonConverter<List<String>> {
+  public DetailsJsonConverter() {
+    super(new TypeReference<List<String>> (){});
+  }
 }
