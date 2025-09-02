881b9310f46b051198d4e89482c77440dd642bd0
NamYunje <namyunje2002@gmail.com>
2025-09-02 18:33:46 +0900

    TORY-163 feat: 유저 정보 가져올 때 이용약관/개인정보/마케팅 동의 일시 추가

--- src/main/java/com/cheftory/api/account/user/dto/UserMeResponse.java
+++ src/main/java/com/cheftory/api/account/user/dto/UserMeResponse.java
@@ -3,11 +3,15 @@ package com.cheftory.api.account.user.dto;
 import com.cheftory.api.account.user.entity.Gender;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record UserMeResponse(
     @JsonProperty("nickname") String nickname,
     @JsonProperty("gender") Gender gender,
-    @JsonProperty("date_of_birth") LocalDate dateOfBirth
+    @JsonProperty("date_of_birth") LocalDate dateOfBirth,
+    @JsonProperty("terms_of_use_agreed_at") LocalDateTime termsOfUseAgreedAt,
+    @JsonProperty("privacy_agreed_at") LocalDateTime privacyAgreedAt,
+    @JsonProperty("marketing_agreed_at") LocalDateTime marketingAgreedAt
 ) {
 
 }
