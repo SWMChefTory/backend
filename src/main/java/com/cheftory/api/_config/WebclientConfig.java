6740263db3dcdc807c761e7773818efbeabc7d8b
banseok1216 <bansuk1216@naver.com>
2025-09-29 20:13:12 +0900

    chore: 외부 api 연결시 timeout 설정

--- src/main/java/com/cheftory/api/_config/WebclientConfig.java
+++ src/main/java/com/cheftory/api/_config/WebclientConfig.java
@@ -1,10 +1,14 @@
 package com.cheftory.api._config;
 
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebclientConfig {
@@ -14,12 +18,30 @@ public class WebclientConfig {
   @Bean
   @Qualifier("recipeCreateClient")
   public WebClient webClientForRecipeServer() {
-    return WebClient.builder().baseUrl(recipeServerUrl).build();
+
+    HttpClient httpClient =
+        HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofSeconds(60));
+
+    return WebClient.builder()
+        .baseUrl(recipeServerUrl)
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .build();
   }
 
   @Bean
   @Qualifier("youtubeClient")
   public WebClient webClientForGoogle() {
-    return WebClient.builder().baseUrl("https://www.googleapis.com/youtube/v3").build();
+
+    HttpClient httpClient =
+        HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofSeconds(20));
+
+    return WebClient.builder()
+        .baseUrl("https://www.googleapis.com/youtube/v3")
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .build();
   }
 }
