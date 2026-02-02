b1ac897c02d3fc42454cbe6d5af03007f5d2472b
banseok1216 <bansuk1216@naver.com>
2026-02-02 20:54:47 +0900

    fix: progressDetail

--- src/main/java/com/cheftory/api/recipe/creation/progress/entity/RecipeProgressDetail.java
+++ src/main/java/com/cheftory/api/recipe/creation/progress/entity/RecipeProgressDetail.java
@@ -3,11 +3,11 @@ package com.cheftory.api.recipe.creation.progress.entity;
 public enum RecipeProgressDetail {
     READY,
     CAPTION,
-    DETAIL,
     TAG,
     DETAIL_META,
     INGREDIENT,
     BRIEFING,
     STEP,
-    FINISHED
+    FINISHED,
+    DETAIL,
 }
